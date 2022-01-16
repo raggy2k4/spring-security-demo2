6fdbe906c17fdcb717b8d2efba64cf23a1edc0e2
Marcin Kowalski <raggy2k4@gmail.com>
2022-01-16 15:20:29 +0100

    add UserDetailsService

--- src/main/java/com/example/springsecuritydemo2/config/WebConfig.java
+++ src/main/java/com/example/springsecuritydemo2/config/WebConfig.java
@@ -1,9 +1,14 @@
 package com.example.springsecuritydemo2.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -14,4 +19,22 @@ public class WebConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers("/all").permitAll().anyRequest().authenticated();
     }
+
+    @Bean
+    protected UserDetailsService userDetailsService(){
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("passuser")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("passadmin")
+                .roles("ADMIN", "USER")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+
 }
